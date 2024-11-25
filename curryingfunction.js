842082068d59bfc4ed69ff36c53726b9ea9aae90
santhoshreddy500939 <santhoshreddy500939@gmail.com>
2024-11-25 08:38:03 +0530

    add:Currying Function

---
+++ curryingfunction.js
@@ -0,0 +1,7 @@
+console.log("Currying Functions in Js..");
+
+//currying takes a function which recieves more than one parameter and 
+        //break it into series of uniary paramter function 
+                //(means function which recieves only one parameter.)
+
+
