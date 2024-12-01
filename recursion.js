c493ee94e63599d478cefd9c3a53ae8c72bff9ce
santhoshreddy500939 <santhoshreddy500939@gmail.com>
2024-12-01 21:00:56 +0530

    add:Recurrsion

--- recursion.js
+++ recursion.js
@@ -64,3 +64,51 @@ console.log(FibPos(3))
 
 
 //https://github.com/gitdagray/js_recursion_examples/blob/main/js/main.js#L50
+
+
+
+const artistsByGenre = {
+    jazz: ["Miles Davis", "John Coltrane"],
+    rock: {
+        classic: ["Bob Seger", "The Eagles"],
+        hair: ["Def Leppard", "Whitesnake", "Poison"],
+        alt: {
+            classic: ["Pearl Jam", "The Killers"],
+            current: ["Joywave", "Sir Sly"]
+        }
+    },
+    unclassified: {
+        new: ["Caamp", "Neil Young"],
+        classic: ["Seal", "Morcheeba", "Chris Stapleton"]
+    }
+}
+
+
+// const getArtistNames = (dataObj, arr = []) => {
+//     Object.keys(dataObj).forEach(key => {
+//         if (Array.isArray(dataObj[key])) {
+//              dataObj[key].forEach(artist => {
+//                 arr.push(artist);
+//             });
+//         }
+//         getArtistNames(dataObj[key], arr);
+//     });
+//     return arr;
+// }
+
+// console.log(getArtistNames(artistsByGenre));
+
+
+const getArtistNames = (dataObj, arr = []) => {
+    Object.keys(dataObj).forEach(key => {
+        if (Array.isArray(dataObj[key])) {
+            return dataObj[key].forEach(artist => {
+                arr.push(artist);
+            });
+        }
+            getArtistNames(dataObj[key], arr);
+        
+    });
+    return arr;
+}
+ console.log(getArtistNames(artistsByGenre));
